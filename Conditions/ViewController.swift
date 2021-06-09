ef82ddc329fde56e395668d84943fbc7db008c11
Robert Mutai <robert.mutai@craftsilicon.com>
2021-06-09 09:38:22 +0300

    Initial Commit

---
+++ Conditions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conditions
+//
+//  Created by MAC USER on 09/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
